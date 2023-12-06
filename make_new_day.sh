e42d53109873585ad343b7a9284eed1c7dcb90e0
Quentin18 <quentindeschamps18@gmail.com>
2023-12-06 20:09:49 +0100

    feat: add make new day script and template

---
+++ make_new_day.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if test -z "$1"; then
+  echo >&2 "usage: ./make_new_day 2023/day01"
+  exit 1
+fi
+
+echo "make day $1"
+
+mkdir $1
+cp template.py $1/part1.py
+touch $1/input.txt
---
+++ template.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    with open("input_test.txt", "r", encoding="utf-8") as file:
+        pass
+
+
+if __name__ == "__main__":
+    main()
